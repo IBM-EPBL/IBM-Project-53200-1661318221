46b0218e589c79061d18da9640807262a4758622
Dhivya Sri S <91817996+DHIVYASRI09@users.noreply.github.com>
2022-10-12 10:34:06 +0530

    Add files via upload
    ASSIGNMENT-2

---
+++ ASSIGNMENT-2/1_format.sql
@@ -0,0 +1,5 @@
+CREATE TABLE format(email,username,rollno,password);
+INSERT INTO format(email,username,rollno,password)VALUES('divyasakthivel706@gmail.com','Dhivya','22','123');
+INSERT INTO format(email,username,rollno,password)VALUES('abinayadevi2002@gmail.com','Abinaya','2','153');
+INSERT INTO format(email,username,rollno,password)VALUES('boomika12@gmail.com','Boomika','13','129');
+INSERT INTO format(email,username,rollno,password)VALUES('darshani33@gmail.com','Darshani','15','323');
---
+++ ASSIGNMENT-2/2_queries.sql
@@ -0,0 +1,4 @@
+UPDATE format SET username='Abinaya Devi' WHERE rollno='2';
+SELECT*FROM format
+DELETE FROM format WHERE username='Darshani';
+SELECT*FROM format
